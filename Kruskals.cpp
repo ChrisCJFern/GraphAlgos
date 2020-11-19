0a0db96c641341d2ed1d8390bdee6254cba15b26
CJ <chriscjfern@hotmail.com>
2020-11-18 21:29:10 -0500

    changes

--- Kruskals.cpp
+++ Kruskals.cpp
@@ -13,20 +13,20 @@ private:
     set<int> vertices;
 
 public:
-    void insertEdge(int from, int to, int weight);
-    void insertVertex(int x);
+    void insertEdgeK(int from, int to, int weight);
+    void insertVertexK(int x);
     int sumOfMST();
 
     // Feel free to add more methods to the API
 };
 
-void Graph::insertEdge(int to, int from, int weight)
+void Graph::insertEdgeK(int to, int from, int weight)
 {
     this->theGraph[to][from] = weight;
     this->theGraph[from][to] = weight;
 }
 
-void Graph::insertVertex(int x)
+void Graph::insertVertexK(int x)
 {
     this->vertices.insert(x);
 }
@@ -47,9 +47,9 @@ int main()
         std::cin >> inVert;
         std::cin >> outVert;
         std::cin >> wt;
-        myGraph->insertEdge(inVert, outVert, wt);
-        myGraph->insertVertex(inVert);
-        myGraph->insertVertex(outVert);
+        myGraph->insertEdgeK(inVert, outVert, wt);
+        myGraph->insertVertexK(inVert);
+        myGraph->insertVertexK(outVert);
     }
     cout << myGraph->sumOfMST();
 }
---
+++ Prims.cpp
@@ -0,0 +1,72 @@
+#pragma once
+#define MAXNUMVERTICES 100
+#include <iostream>
+#include <set>
+#include <vector>
+#include <map>
+#include <climits>
+using namespace std;
+
+
+class Graph
+{
+private:
+    int theGraph[MAXNUMVERTICES][MAXNUMVERTICES] = { 0 };
+    set<int> vertices;
+
+public:
+    void insertEdge(int from, int to, int weight);
+    void insertVertex(int x);
+    void primMST();
+    // Feel free to add more methods to the API
+};
+
+void Graph::insertEdge(int from, int to, int weight)
+{
+    this->theGraph[to][from] = weight;
+    this->theGraph[from][to] = weight;
+}
+
+void Graph::insertVertex(int x)
+{
+    this->vertices.insert(x);
+}
+
+void Graph::primMST()
+{
+    set<int> inSpanningTree;
+    // find minimum edge
+    int minEdge = INT_MAX;
+    int minFrom = -1;
+    int minTo = -1;
+    for (auto from : vertices) {
+        for (auto to : vertices) {
+            if (theGraph[from][to] != 0 && theGraph[from][to] < minEdge) {
+                minEdge = theGraph[from][to];
+                minFrom = from;
+                minTo = to;
+            }
+        }
+    }
+    vertices.erase(minFrom);
+    vertices.erase(minTo);
+    cout << minFrom << "|" << minTo << "|" << minEdge << endl;
+    //your code here        
+}
+
+int main()
+{
+    Graph* myGraph = new Graph();
+    int numEdges, inVert, outVert, wt;
+    std::cin >> numEdges;
+    for (int i = 0; i < numEdges; i++)
+    {
+        std::cin >> inVert;
+        std::cin >> outVert;
+        std::cin >> wt;
+        myGraph->insertEdge(inVert, outVert, wt);
+        myGraph->insertVertex(inVert);
+        myGraph->insertVertex(outVert);
+    }
+    myGraph->primMST();
+}
